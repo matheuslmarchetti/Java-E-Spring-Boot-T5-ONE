115506da928c030d9e9a3149a596795a45a04eb4
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-08-09 21:49:20 -0400

    creating exemple connection test

--- cursojavaejbdctrabalhandocomumbancodedados/src/br/com/loja/virtual/repository/test/TestaConexao.java
+++ cursojavaejbdctrabalhandocomumbancodedados/src/br/com/loja/virtual/repository/test/TestaConexao.java
@@ -1,9 +1,18 @@
 package br.com.loja.virtual.repository.test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class TestaConexao {
 
-	public static void main(String[] args) {
-		System.out.println("Test!");
+	public static void main(String[] args) throws SQLException {
+		Connection connection = DriverManager
+					.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", 
+							"marchetticodes", "mysql@ubuntu@marchetti@codes");
+		System.out.println("Fechando Conexão!!");
+		
+			connection.close();
 
 	}
 
