1a3dab48394dfc39326b5066d2e5ea1520c235b0
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-08-13 15:13:04 -0400

    Controle de transaçao

--- cursojavaejbdctrabalhandocomumbancodedados/src/br/com/loja/virtual/repository/test/TestaInsercaoComParametro.java
+++ cursojavaejbdctrabalhandocomumbancodedados/src/br/com/loja/virtual/repository/test/TestaInsercaoComParametro.java
@@ -11,25 +11,43 @@ import br.com.loja.virtual.repository.conexao.ConnectionFactory;
 public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
-		String nome = "Teclado'";
-		String descricao = "Teclado Sem Fio): delete;";
 		ConnectionFactory connectionFactory = new ConnectionFactory();
-		Connection connection = connectionFactory.recuperarConexao();
-		
-		PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao)"
-				+ "VALUES (?, ?)",
-				Statement.RETURN_GENERATED_KEYS);
-		preparedStatement.setString(1, nome);
-		preparedStatement.setString(2, descricao);
-		
-		preparedStatement.execute();
-		
-		ResultSet resultSet = preparedStatement.getGeneratedKeys();
-		
-		while (resultSet.next()) {
-			int id = resultSet.getInt(1);
-			System.out.println("O id criado foi: " + id);
+		try(Connection connection = connectionFactory.recuperarConexao()){
+			connection.setAutoCommit(false);
+	
+			try(PreparedStatement preparedStatement = connection.prepareStatement(
+					"INSERT INTO PRODUTO (nome, descricao)" + "VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS)){
+				adicionarVariavel("SmartTV", "45 Polegadas", preparedStatement);
+				adicionarVariavel("Rádio", "Rádio de Bateria", preparedStatement);
+	
+				connection.commit();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("ROLLBACK EXECUTADO");
+				connection.rollback();
+			}
 		}
 	}
 
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement preparedStatement)
+			throws SQLException {
+		preparedStatement.setString(1, nome);
+		preparedStatement.setString(2, descricao);
+
+		
+		 if(nome.equals("Rádio")) { 
+			 throw new RuntimeException("Não foi possível adicionar o produto"); 
+		}
+		 
+
+		preparedStatement.execute();
+
+		try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+			while (resultSet.next()) {
+				int id = resultSet.getInt(1);
+				System.out.println("O id criado foi: " + id);
+			}
+		}
+	}
 }
