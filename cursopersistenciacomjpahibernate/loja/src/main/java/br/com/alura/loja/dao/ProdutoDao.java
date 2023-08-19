dacb2701aeb72da6026a0d11eca9979af7ce9437
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-08-19 00:08:51 -0400

    Consultas com JPQL

--- cursopersistenciacomjpahibernate/loja/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ cursopersistenciacomjpahibernate/loja/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -1,5 +1,7 @@
 package br.com.alura.loja.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.alura.loja.modelo.Produto;
@@ -14,5 +16,23 @@ public class ProdutoDao {
 	
 	public void cadastrar(Produto produto) {
 		this.entityManager.persist(produto);
-	}	
+	}
+
+	public void atualizar(Produto produto) {
+		this.entityManager.merge(produto);
+	}
+	
+	public void remover(Produto produto) {
+		produto = entityManager.merge(produto);
+		this.entityManager.remove(produto);
+	}
+	
+	public Produto buscarPorId(Long id) {
+		return entityManager.find(Produto.class, id);
+	}
+	
+	public List<Produto> buscarTodos() {
+		String jpql = "SELECT p FROM Produto p";
+		return entityManager.createQuery(jpql, Produto.class).getResultList();
+	}
 }
--- cursopersistenciacomjpahibernate/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ cursopersistenciacomjpahibernate/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,6 +1,7 @@
 package br.com.alura.loja.testes;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
@@ -14,6 +15,18 @@ public class CadastroDeProduto {
 
 	public static void main(String[] args) {
 		
+		cadastrarProduto();
+		EntityManager entityManager = JPAUtil.getEntityManager();
+		ProdutoDao produtoDao = new ProdutoDao(entityManager);
+		
+		Produto produto = produtoDao.buscarPorId(1l);
+		System.out.println(produto.getPreco());
+		
+		List<Produto> todos = produtoDao.buscarTodos();
+		todos.forEach(p -> System.out.println(p.getNome()));
+	}
+
+	private static void cadastrarProduto() {
 		Categoria celulares = new Categoria("CELULARES");
 		Produto celular = new Produto("Xiaomi Redmi", "12gb de RAM", new BigDecimal("1200"), celulares);
 		
