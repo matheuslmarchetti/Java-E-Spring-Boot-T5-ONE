4154d9e5e7160cff3632b2924bee54f7d81f590e
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-08-28 21:43:15 -0400

    Cancelamento de consultas

--- cursospringboot3desenvolvaumaAPIRestemjava/api/src/main/java/med/voll/api/controller/ConsultaController.java
+++ cursospringboot3desenvolvaumaAPIRestemjava/api/src/main/java/med/voll/api/controller/ConsultaController.java
@@ -3,6 +3,7 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.domain.consulta.AgendaDeConsultas;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
 import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -21,5 +22,12 @@ public class ConsultaController {
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados){
         agenda.agendar(dados);
         return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
+    }
+
+    @DeleteMapping
+    @Transactional
+    public  ResponseEntity cancelar(@RequestBody @Valid DadosCancelamentoConsulta dados){
+        agenda.cancelar(dados);
+        return ResponseEntity.noContent().build();
     }
 }
--- cursospringboot3desenvolvaumaAPIRestemjava/api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ cursospringboot3desenvolvaumaAPIRestemjava/api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -28,7 +28,7 @@ public class AgendaDeConsultas {
         }
         var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
         var medico = escolherMedico(dados);
-        var consulta = new Consulta(null, medico, paciente, dados.data());
+        var consulta = new Consulta(null, medico, paciente, dados.data(), null);
         consultaRepository.save(consulta);
     }
 
@@ -40,5 +40,13 @@ public class AgendaDeConsultas {
             throw new ValidacaoException("Especialidade é obrigatório, quando médico não for escolhido!");
         }
         return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
+    }
+
+    public void cancelar(DadosCancelamentoConsulta dados) {
+        if (!consultaRepository.existsById(dados.idConsulta())) {
+            throw new ValidacaoException("Id da consulta informado não existe!");
+        }
+        var consulta = consultaRepository.getReferenceById(dados.idConsulta());
+        consulta.cancelar(dados.motivo());
     }
 }
