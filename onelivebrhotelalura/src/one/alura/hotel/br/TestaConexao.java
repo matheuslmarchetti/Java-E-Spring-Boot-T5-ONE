d5dcf51d33c25ccf94d3455e1ddccd2507dc4c1d
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-09-19 09:26:04 -0400

    Preparando o projeto

--- onelivebrhotelalura/src/one/alura/hotel/br/TestaConexao.java
+++ onelivebrhotelalura/src/one/alura/hotel/br/TestaConexao.java
@@ -1,9 +1,18 @@
 package one.alura.hotel.br;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class TestaConexao {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws SQLException {
+		Connection conexao = DriverManager.getConnection(
+				"jdbc:mysql://localhost/hotel_alura?useTimezone=true&serverTimezone=UTC", 
+				"marchetticodes", 
+				"marchetticodes");
+		System.out.println("Testando conexão!");
+		conexao.close();
 
 	}
 
